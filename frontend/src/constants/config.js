5dd3238a702777d2100db56710bfccf7b462c145
totop716 <totop716@gmail.com>
2020-01-11 02:44:51 +0800

    manually add default=django.utils.timezone.now to migrations for updated_at new field

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -4,8 +4,8 @@
 export default {
   // will use depending on the mode
   API_URL: {
-    DEV: 'https://ugg-2957.botics.co/',
-    // DEV: 'http://localhost:8000/',
+    // DEV: 'https://ugg-2957.botics.co/',
+    DEV: 'http://localhost:8000/',
     PROD: ''
   }
 };
