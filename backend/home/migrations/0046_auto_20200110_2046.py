fcf673027d526389a97c8692ba521c79d8c531a4
totop716 <totop716@gmail.com>
2020-01-15 02:06:12 +0800

    revert original 0046 migration to sync with actual model

--- backend/home/migrations/0046_auto_20200110_2046.py
+++ backend/home/migrations/0046_auto_20200110_2046.py
@@ -14,4 +14,15 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AddField(
+            model_name='survey',
+            name='created_at',
+            field=models.DateTimeField(auto_now_add=True, default=django.utils.timezone.now),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='survey',
+            name='updated_at',
+            field=models.DateTimeField(auto_now=True),
+        )
     ]
